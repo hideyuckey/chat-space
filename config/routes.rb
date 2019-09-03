d7a52b73a44c08a1d52f6f61ccad63e802c6ca1e
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2019-09-03 19:14:19 +0900

    first-commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :message, only: [:index, :create]
+    
   end
 end
