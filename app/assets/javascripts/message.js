88950fb8b2b813bacc60a5967da02e941e9c917f
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2019-09-07 14:06:57 +0900

    send-message-fo-json

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,37 @@
 $(function() {
+
+  function buildHTML(message){
+    var html = `<div class = "message">
+                  <div class = "message__name">${ message.user_name }</div>
+                  <div class = "message__date">${ message.updated_at }</div>
+                  <div class = "message__text">${ message.content }</div>
+                  ${message.image.persent ? '<img class = "message__image", src = "${ message.image.url }"></img>' : ''}
+                </div>`
+    return html;
+  }
+
+
   $("#new_message").on("submit", function(e){
     e.preventDefault();
     let formData = new FormData(this)
-    href = window.location.href
+    var url = $(this).attr('action')
     $.ajax({
-      url: href,
+      url: url,
       type: "POST",
       data: formData,
-      dataType: "json",
+      dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      console.log(data)
+      var html = buildHTML(data);
+      $(".chat-main__chat-list").append(html)
+      $('.chat-main__chat-list').animate({ scrollTop: $('.chat-main__chat-list')[0].scrollHeight });
+      $(".input__text").val('');
+    })
+    .fail(function(){
+      alert("error");
     })
   })
 })
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+
+json.content    @message.content
+json.image      @message.image
+json.user_name  @message.user.name
+json.updated_at  @message.updated_at.strftime("%Y/%m/%d %H:%M")
