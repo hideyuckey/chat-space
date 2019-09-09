2dc4c22ba86aea834507c5a1d5bcf16625af8031
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2019-09-09 14:25:31 +0900

    add-chat-menber

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -5,8 +5,8 @@ $(function() {
   function appendUser(user) {
     let html = `
               <div class="chat-group-user clearfix">
-              <p class="chat-group-user__name">${user.name}</p>
-              <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                <p class="chat-group-user__name">${user.name}</p>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
               </div>`
     search_list.append(html);
   }
@@ -18,10 +18,23 @@ $(function() {
     search_list.append(html);
   }
 
+  let add_list = $("#user-search-add")
+
+  function addGroupUser(user) {
+    let userId = $(user).data("user-id")
+    let userName = $(user).data("user-name")
+
+    let htmlUser = `
+                <div class='chat-group-user'>
+                  <input name='group[user_ids][]' type='hidden' value='${userId}'>
+                  <p class='chat-group-user__name'>${userName}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+    add_list.append(htmlUser);
+  }
 
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
-    console.log(input);
     $.ajax({
       type: "GET",
       url: "/users",
@@ -29,12 +42,16 @@ $(function() {
       dataType: "json"
     })
     .done(function(users) {
-      console.log(users)
       $("#user-search-result").empty();
       if (users.length !== 0) {
         users.forEach(function(user) {
           appendUser(user);
         });
+        $(".chat-group-user").on("click", ".user-search-add", function() {
+          $(this).parent().remove();
+          let user = $(this)
+          addGroupUser(user)
+        })
       }
       else {
         appendErrMsgToHTML("一致するユーザーはいません")
